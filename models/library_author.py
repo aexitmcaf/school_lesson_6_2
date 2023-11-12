6172d75ac98cbe4f75cf6d823560541d6a981fbb
Oleksandr <alexitmcaf@gmail.com>
2023-11-11 22:33:23 -0400

    [ADD] first last name fields add translate attribute

--- models/library_author.py
+++ models/library_author.py
@@ -5,8 +5,8 @@ class LibraryAuthor(models.Model):
     _name = 'library.author'
     _description = 'Library Book Authors'
 
-    first_name = fields.Char(required=True, string=_("First name"))
-    last_name = fields.Char(required=True, string=_("Last name"))
+    first_name = fields.Char(required=True, string=_("First name"), translate=True)
+    last_name = fields.Char(required=True, string=_("Last name"), translate=True)
     birth_date = fields.Date('Birthday')
 
     def name_get(self):
